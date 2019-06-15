e3c3c188c47c64223df1b8562600d3dec93a59e2
Mayra Cademartori <44714425+prphawk@users.noreply.github.com>
2019-06-14 21:54:10 -0300

    Esboço 2ª Pesquisas

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 # coding=UTF-8
-from trie import Trie
+from hash import *
+from trie import *
+from input import *
+
+def searchTitle(trie, hashMovie):
+
+	prefix = input("Insira nome ou prefixo do filme: ")
+	while prefix != 'quit':
+		for MovieID in trie.search(prefix):
+			print(hashMovie.search(MovieID))
+		prefix = input("\nInsira nome ou prefixo do filme: ")
+
+def searchUser(hashMovie, hashUser):
+
+	#userID = input("Insira ID do usuario: ")
+	user = hashUser.search(48644)
+	print(user)
+
+	#while userID != 'quit':
+	for MovieID,rating in user.userRat.items():
+		node = hashMovie.search(MovieID)
+		print(node.title,MovieID,rating)
+	prefix = input("\nInsira nome ou prefixo do filme: ")
+
 
 def main():
-	trie = Trie()
+	
+	trie, hashMovie, hashUser = openF()
+	# searchTitle(trie, hashMovie)
+	searchUser(hashMovie,hashUser)
 
-	trie.insert("Teste número 0", 0)
-	trie.insert("Tentativa número 2", 2)
-	trie.insert("Traidor número 3", 3)
-	trie.insert("Teste dfsfhkj 5", 5)
-	
-	print(trie.search("Teste nú"))
-	
+
+
+
 if __name__ == "__main__":
     main()
