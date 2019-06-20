236d180e96f6379983708242adb4dccad2426840
Mayra Cademartori <44714425+prphawk@users.noreply.github.com>
2019-06-20 18:18:56 -0300

    fewer functions

--- hashMovie.py
+++ hashMovie.py
@@ -27,22 +27,21 @@ class HashMovie:
 
 		self.taken = 0
 
-		self.rate = (self.taken / self.size)*100
-		
 
 	def insert(self, movieID, title, genres, rating):
 		
 		t = 0
 		code = movieID % self.size
+		
 		while self.table[code] != None:
+
 			if movieID == self.table[code].movieID:
 				self.nodeUpdt(code, rating)
 				return
 			else: code, t = self.colision(code, t)
 
 		self.table[code] = HashMovieNode(movieID, title, genres, rating)
-		self.incTaken()
-		#self.reSize()
+		self.taken += 1
 
 	def colision(self, code, t):
 
@@ -50,28 +49,16 @@ class HashMovie:
 		code = int(code + 0.5*t + 0.5*t*t) % self.size
 		return code, t
 
+
 	def nodeUpdt(self, code, rating):
 	
 		if self.table[code].meanRat == None:
 			self.table[code].meanRat = rating
-		else: self.table[code].meanRat = (rating + self.table[code].meanRat)/2
+
+		else: 
+			self.table[code].meanRat = (rating + self.table[code].meanRat)/2
 		
 		self.table[code].count += 1
-
-
-	def reSize(self):
-
-		if self.rate >= 67:
-			listAux = [None]*self.size 
-			self.table.extend(listAux) 
-			del listAux
-			self.size = len(self.table)
-			self.rate = (self.taken/self.size)*100
-
-
-	def incTaken(self):
-		self.taken = self.taken + 1
-		self.rate = (self.taken/self.size)*100
 
 
 	def printHash(self):
@@ -81,4 +68,4 @@ class HashMovie:
 
 
 	def __str__(self):
-		return " * SIZE: {} | TAKEN: {} | RATE: {}".format(self.size, self.taken, self.rate)
+		return " * SIZE: {} | TAKEN: {} | RATE: {}".format(self.size, self.taken, (self.taken/self.size)*100)
