6a374f1357cc76dcc3a5aed42839652c33c51f1a
Mayra Cademartori <44714425+prphawk@users.noreply.github.com>
2019-06-20 05:32:42 -0300

    new hashGenre

--- input.py
+++ input.py
@@ -2,28 +2,37 @@ from hash import *
 from trie import *
 import csv
 
+def takeRatings(elem):
+    return elem.meanRat
+
 def openF():
 
 	hashMovie = HashMovie()
 	hashUser = HashUser()
+	hashGenre = HashGenre()
 	trie = Trie()
 
+	frat =  open('minirating.csv', 'r', encoding="utf8") 
 	fmovie = open('movie.csv', 'r', encoding="utf8")
 	csv_fmovie = csv.reader(fmovie)
 	next(csv_fmovie)
+	next(frat)
+
 	for line in csv_fmovie:
+
 		movieID = int(line[0])
 		title = line[1]
+
 		if line[2] != "(no genres listed)":
 			genres = line[2].split("|")
-		else:genres = []
+		else: genres = None
+
 		trie.insert(title,movieID)
 		hashMovie.insert(movieID, title, genres, None)
 	
-	frat =  open('minirating.csv', 'r', encoding="utf8") 
-	next(frat)
 
 	for line in frat:
+
 		line = line.split(",")
 		userID = int(line[0])
 		movieID = int(line[1])
@@ -34,15 +43,16 @@ def openF():
 	frat.close()
 	fmovie.close()
 
-	hashUser.printHash()
+	for movie in hashMovie.table:
+		if movie != None and movie.genres != None and movie.count > 10:
+			for genre in movie.genres:
+				hashGenre.insert(genre, movie)
+
+	for node in hashGenre.table:
+		if node != None:
+			node.movies.sort(key=takeRatings, reverse=True)
+
+
+	return trie, hashMovie, hashUser, hashGenre
+
 		
-	
-
-def main():
-
-	openF()
-
-
-
-if __name__ == "__main__":
-	main()
